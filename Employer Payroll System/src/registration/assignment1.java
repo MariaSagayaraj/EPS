bd17e7b5e32153ab010f905d1c88ed9c088126e4
Abinaya.Murugesan <Abinaya.Murugesan@B108-09.soit.local>
2018-07-31 09:56:09 +1200

    Added assignment1.java

---
+++ Employer Payroll System/src/registration/assignment1.java
@@ -0,0 +1,157 @@
+package LOGIN;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JRadioButton;
+import javax.swing.JToggleButton;
+import javax.swing.JSeparator;
+import java.awt.Color;
+import javax.swing.border.LineBorder;
+import org.eclipse.wb.swing.FocusTraversalOnArray;
+import java.awt.Component;
+import java.awt.Font;
+
+public class assignment1 extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
+	private JTextField textField_5;
+	private JTextField textField_6;
+	private JTextField textField_7;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					assignment1 frame = new assignment1();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public assignment1() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 559);
+		contentPane = new JPanel();
+		contentPane.setForeground(Color.WHITE);
+		contentPane.setBackground(Color.PINK);
+		contentPane.setBorder(new LineBorder(new Color(0, 0, 0), 2, true));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("PAYMENT CALCULATION");
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblNewLabel.setBounds(150, 11, 188, 14);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Employee ID");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblNewLabel_1.setBounds(44, 55, 97, 14);
+		contentPane.add(lblNewLabel_1);
+		
+		textField = new JTextField();
+		textField.setBounds(228, 52, 97, 20);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel_2 = new JLabel("Time in");
+		lblNewLabel_2.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblNewLabel_2.setBounds(44, 100, 80, 14);
+		contentPane.add(lblNewLabel_2);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(228, 97, 97, 20);
+		contentPane.add(textField_1);
+		textField_1.setColumns(10);
+		
+		JLabel lblNewLabel_3 = new JLabel("Time out");
+		lblNewLabel_3.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblNewLabel_3.setBounds(44, 146, 55, 14);
+		contentPane.add(lblNewLabel_3);
+		
+		textField_2 = new JTextField();
+		textField_2.setBounds(228, 143, 97, 20);
+		contentPane.add(textField_2);
+		textField_2.setColumns(10);
+		
+		JLabel lblNewLabel_4 = new JLabel("Total no.of hours worked");
+		lblNewLabel_4.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblNewLabel_4.setBounds(44, 190, 140, 14);
+		contentPane.add(lblNewLabel_4);
+		
+		textField_3 = new JTextField();
+		textField_3.setBounds(228, 184, 97, 20);
+		contentPane.add(textField_3);
+		textField_3.setColumns(10);
+		
+		JLabel lblNewLabel_5 = new JLabel("Pay Rate / hour");
+		lblNewLabel_5.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblNewLabel_5.setBounds(44, 225, 97, 14);
+		contentPane.add(lblNewLabel_5);
+		
+		textField_4 = new JTextField();
+		textField_4.setBounds(228, 228, 97, 20);
+		contentPane.add(textField_4);
+		textField_4.setColumns(10);
+		
+		JLabel lblNewLabel_6 = new JLabel("Duration of Work");
+		lblNewLabel_6.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblNewLabel_6.setBounds(44, 272, 122, 14);
+		contentPane.add(lblNewLabel_6);
+		
+		JSeparator separator = new JSeparator();
+		separator.setBounds(98, 312, 1, 2);
+		contentPane.add(separator);
+		
+		JLabel lblNewLabel_7 = new JLabel("From");
+		lblNewLabel_7.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblNewLabel_7.setBounds(44, 312, 46, 14);
+		contentPane.add(lblNewLabel_7);
+		
+		textField_5 = new JTextField();
+		textField_5.setBounds(228, 312, 97, 20);
+		contentPane.add(textField_5);
+		textField_5.setColumns(10);
+		
+		JLabel lblTo = new JLabel("To");
+		lblTo.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblTo.setBounds(44, 349, 46, 14);
+		contentPane.add(lblTo);
+		
+		textField_6 = new JTextField();
+		textField_6.setBounds(228, 346, 97, 20);
+		contentPane.add(textField_6);
+		textField_6.setColumns(10);
+		
+		JLabel lblNewLabel_8 = new JLabel("Total Pay");
+		lblNewLabel_8.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblNewLabel_8.setBounds(44, 447, 80, 14);
+		contentPane.add(lblNewLabel_8);
+		
+		textField_7 = new JTextField();
+		textField_7.setBounds(228, 444, 97, 20);
+		contentPane.add(textField_7);
+		textField_7.setColumns(10);
+		contentPane.setFocusTraversalPolicy(new FocusTraversalOnArray(new Component[]{lblNewLabel, lblNewLabel_1, textField, lblNewLabel_2, textField_1, lblNewLabel_3, textField_2, lblNewLabel_4, textField_3, lblNewLabel_5, textField_4, lblNewLabel_6, separator, lblNewLabel_7, textField_5, lblTo, textField_6, lblNewLabel_8, textField_7}));
+	}
+}
