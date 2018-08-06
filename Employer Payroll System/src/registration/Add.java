0cb74576c087eb6880bfdb15f632afdfa1195b08
Maria.Sagayaraj <Maria.Sagayaraj@B108-10.soit.local>
2018-08-07 10:12:13 +1200

    Add Commmit_v2

--- Employer Payroll System/src/registration/Add.java
+++ Employer Payroll System/src/registration/Add.java
@@ -21,7 +21,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JLabel;
 
-public class Employee_registration extends JFrame {
+public class Add extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField textField;
